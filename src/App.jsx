ac96a8d007732eba205b90925b657db53a89c415
Somavik99 <94855290+Somavik99@users.noreply.github.com>
2023-03-25 15:44:43 +0530

    State applied

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import React from "react";
-
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import SignUpUI from "./Components/SignUpUI/SignUpUI";
 
 function App() {
-  return <div className="App" > <h1>Register for Online Galary</h1> </div>;
+  return (
+    <div className="App">
+      <h1>Register for Online Galary</h1>
+      <div>
+        <Routes>
+          <Route path="/" element={<SignUpUI />} />
+        </Routes>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/Components/SignUpUI/SignUpUI.jsx
+++ src/Components/SignUpUI/SignUpUI.jsx
@@ -1,21 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SignUpUI = () => {
+  const [SignUpVal, setSignUpVal] = useState({
+    user: "",
+    email: "",
+    P: "",
+    phN: "",
+    cP: "",
+  });
   return (
-    <from>
+    <form>
       <div>
         <span>Name:</span>
         <span>
-          <input type="text" name="name" />
+          <input type="text" name="user" />
         </span>
       </div>
       <div>
-        <span>Email</span>
+        <span>Emai:l</span>
         <span>
           <input type="email" name="email" />
         </span>
       </div>
-    </from>
+      <div>
+        <span>Phone No.:</span>
+        <span>
+          <input type="number" name="PhN" />
+        </span>
+      </div>
+      <div>
+        <span>Password:</span>
+        <span>
+          <input type="password" name="P" />
+        </span>
+      </div>
+      <div>
+        <span>Confirm Password</span>
+        <span>
+          <input type="password" name="cP" />
+        </span>
+      </div>
+      <div>
+        <button>Next</button>
+      </div>
+    </form>
   );
 };
 
